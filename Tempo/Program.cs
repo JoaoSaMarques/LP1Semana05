e4338a5988d02f29494c156b9401247bf78cd8ce
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-28 19:21:43 +0100

    Added second crono start

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -12,11 +12,14 @@ namespace Tempo
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
 
-            //Inicia contagem
+            //Inicia contagem 1
             crono1.Start();
 
             //Faz pausa ao programa por 0,5 segundos
             Thread.Sleep(500);
+
+            //Inicia contagem 2 
+            crono2.Start();
         }
     }
 }
