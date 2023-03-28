c0a4e632938a4296d21f2119afe9fc8ef5cae26c
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-28 19:20:54 +0100

    Added timer sleep for 0.5 seconds

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -12,7 +12,11 @@ namespace Tempo
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
 
+            //Inicia contagem
             crono1.Start();
+
+            //Faz pausa ao programa por 0,5 segundos
+            Thread.Sleep(500);
         }
     }
 }
