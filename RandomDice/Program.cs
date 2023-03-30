4ad0e6cc5a0142a709538dee2a4e39bcc4c04076
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-30 15:17:08 +0100

    Limitador

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -10,6 +10,12 @@ namespace RandomDice
             Console.WriteLine("Input the number of the dice: ");
             //Converter a string em inteiro.
             int n = int.Parse(Console.ReadLine());
+
+            //Vai deixar-nos fazer um limitador entre 1 e 6.
+            for (int i = 0; i < n; i++)
+            {
+                int dice = 
+            }
         }
     }
 }
