8c28097853aac61183ded8aa4736cefcb463b9f6
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-30 15:19:39 +0100

    Better comment and started random value for each dice

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -6,10 +6,16 @@ namespace RandomDice
     {
         static void Main(string[] args)
         {
-            //Valor do número vai ser utilizado para o resto do programa.
+            //Quantidade de dados lançados.
             Console.WriteLine("Input the number of the dice: ");
             //Converter a string em inteiro.
             int n = int.Parse(Console.ReadLine());
+
+            //Precisamos disto quando tivermos que sumar o valor dado.
+            int sum = 0;
+
+            //Temos que criar um valor entre 1 e 6 para cada dado lançado.
+            Random value = new Random()
 
             //Vai deixar-nos fazer um limitador entre 1 e 6.
             for (int i = 0; i < n; i++)
