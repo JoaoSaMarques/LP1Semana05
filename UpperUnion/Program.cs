6adec217198fb9355ce1b3483300a6248fb6cb06
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-31 10:22:23 +0100

    added alphabetical sorter

--- UpperUnion/Program.cs
+++ UpperUnion/Program.cs
@@ -11,10 +11,24 @@ namespace UpperUnion
             Console.WriteLine("Write a sentence");
             //Read string of user.
             string string1 = (Console.ReadLine());
+
             //Convert string to uppercase.
             string upperString = string1.ToUpper();
+
+            //copy upperstring into a separate string.
+            string x = upperString;
+
+            //convert string x to char array
+            char[] charX = x.ToCharArray();
+
+            //Sort array alphabetically
+            Array.Sort(charX);
+
             //Print upper version.
             Console.WriteLine(upperString);
+            //Print alphabetical order.
+            Console.WriteLine(charX);
+
         }
     }
 }
