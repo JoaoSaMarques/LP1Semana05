0d59a070608b588a444ae673b39e6b3d64342e4e
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-20 11:29:12 +0000

    Added constructor

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,6 +4,16 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        public string name;
+        public int health;
+        public int shield;
+
+        public Enemy(string name)
+        {
+            this.name = name; // name is assigned to this.name
+            health = 100;
+            shield = 0;
+        }
+
     }
 }
