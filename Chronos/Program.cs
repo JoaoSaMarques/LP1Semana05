d4835482d5809ef01f2bc00ee63a1761e938e1e1
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-20 11:00:19 +0000

    time demonstrated

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -19,6 +19,18 @@ namespace Chronos
 
             crono1.Stop();
             crono2.Stop();
+
+            TimeSpan c1 = crono1.Elapsed;
+            TimeSpan c2 = crono2.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string Elapsed1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                c1.Hours, c1.Minutes, c1.Seconds, c1.Milliseconds / 10);
+            Console.WriteLine("RunTime " + Elapsed1);
+
+            string Elapsed2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                c2.Hours, c2.Minutes, c2.Seconds, c2.Milliseconds / 10);
+            Console.WriteLine("RunTime " + Elapsed2);
         }
     }
 }
