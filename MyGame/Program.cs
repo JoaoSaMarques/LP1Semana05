8e4c17adb2ced6227ca633afad3921e453abc124
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-20 12:05:38 +0000

    accept number of enemies in args

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,14 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Number of enemies not provided.");
+                return;
+            }
+
+            int numEnemies = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[numEnemies];
         }
     }
 }
