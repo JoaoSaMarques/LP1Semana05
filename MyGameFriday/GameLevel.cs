a603eefabdebac9e9fd3a5639ef5172a72366d09
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-22 13:27:47 +0000

    added getdifficulty

---
+++ MyGameFriday/GameLevel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MyGameFriday
+{
+    public class GameLevel
+    {
+        private Enemy[] _rooms;
+        private Difficulty _difficulty;
+
+        public GameLevel(int numRooms, Difficulty difficulty)
+        {
+            _rooms = new Enemy[numRooms];
+            _difficulty = difficulty;
+        }
+
+        public Difficulty GetDifficulty()
+        {
+            return _difficulty;
+        }
+
+
+    }
+}
