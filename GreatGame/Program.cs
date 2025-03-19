bdc6bd72ed24bf73f03c09b470f348ed7e87f74b
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-19 14:07:06 +0000

    Add initial code

---
+++ GreatGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreatGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
