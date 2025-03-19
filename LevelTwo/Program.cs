81b2a4babac6eb5ca131d23b12f8c459e6aac354
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-19 14:30:05 +0000

    add new requirements

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -6,7 +6,7 @@ namespace LevelTwo
     {
         private static void Main(string[] args)
         {
-            CanvasImage image = new CanvasImage("tux.jpg");
+            CanvasImage image = new CanvasImage("../tux.jpg"); 
 
             // Set the max width of the image
             image.MaxWidth(24);
